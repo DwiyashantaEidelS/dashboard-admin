a4fc56142daf4c6d5e7df182babbac54a4afd3c1
eidelsafira <dwiyashanta@gmail.com>
2026-05-08 02:14:07 +0700

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span>Dashboard Admin</span>
+            <span>Del Admin Dashboard</span>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import "./navbar.scss"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            
+            <div className="logo">
+                <img src="/logo.svg" alt="" />
+                <span>Dashboard Admin</span>
+            </div>
+            
+            <div className="icons">
+                <img src="/search.svg" alt="" className="icons" />
+                <img src="/app.svg" alt="" className="icons" />
+                <img src="/expand.svg" alt="" className="icons" />
+
+                <div className="notification">
+                    <img src="/notifications.svg" alt="" />
+                    <span>1</span>
+                </div>
+
+                <div className="user">
+                    <img
+                        src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                        alt=""
+                    />
+                    <span>Jane</span>
+                </div>
+                
+                <img src="/settings.svg" alt="" className="icons" />
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
